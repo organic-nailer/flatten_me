0051f92ef9dbb935bfb41b904165cf8de4bf83a1
速川湧気 <hykwyuk0125@gmail.com>
2022-02-07 14:21:23 +0900

    cross platform

--- android/app/src/main/kotlin/com/example/flatter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flatter/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flatter
+package dev.fastriver.flattenme
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flatten me!',
       theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.green,
           textTheme: GoogleFonts.shareTechMonoTextTheme()),
       home: const StartPage(),
     );
